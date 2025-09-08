d874caa63644c6c290f319016f190b59b510ce52
S M Ishtiak Mahmud Nihal <shakilmahmud.sm10@gmail.com>
2025-09-08 17:57:51 +0600

    Update splash_screen1.dart

--- lib/task5/splash_screen/splash_screen1.dart
+++ lib/task5/splash_screen/splash_screen1.dart
@@ -101,7 +101,7 @@ class _SplashScreenState extends State<SplashScreen>
         Navigator.pushReplacement(
           context,
           MaterialPageRoute(
-            builder: (context) => const QMOnboardingPage5(),
+            builder: (context) => const QMOnboardingPage4(),
           ),
         );
       }
